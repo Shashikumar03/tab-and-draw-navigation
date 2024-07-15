c9c803e5aeb002e27642cbdefe6c92b6bac273c6
shashCard91 <shashi.kushwaha@card91.io>
2024-07-15 15:19:47 +0530

    Drawer and tab navugation

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab=createBottomTabNavigator()
+
+const Drawer = createDrawerNavigator();
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./components/Home"
+import AppSetting from "./components/AppSetting"
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen 
+        name="Home"
+         component={Home}
+         options={{
+          title:"Dashboard",
+          drawerLabel:"Dashboard label",
+          drawerActiveTintColor:"#333",
+          drawerActiveBackgroundColor:"lightblue",
+          drawerContentStyle:{
+            backgroundColor:"lightgreen"
+          }
+         }}
+         />
+        <Drawer.Screen name="AppSetting" component={AppSetting} />
+      </Drawer.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="AppSetting" component={AppSetting} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
---
+++ components/AppSetting.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import { Text, View } from "react-native";
+
+export default function AppSetting() {
+  return (
+    <View>
+        <Text>appSetting</Text>
+    </View>
+  )
+}
+
+ 
